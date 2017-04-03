03cd33acb5d58806a6a16858385b4aff63f24123
ken <block24block@gmail.com>
2017-04-03 18:06:38 +0800

    remove my_chart

--- lib/yinx_st.rb
+++ lib/yinx_st.rb
@@ -1,28 +1,12 @@
 require "yinx_st/version"
 require 'yinx'
 require 'yinx_sql'
-require 'my_chart'
 require 'time_seq'
 
 require 'yinx_st/note_meta'
 require 'yinx_st/batches'
 
 require 'my_chartkick'
-
-class Yinx::NoteMeta
-  def stack_book
-    st = stack.nil? ? '' : "#{stack}/"
-    "#{st}#{book}"
-  end
-
-  def stack_name
-    stack ? stack : 'No Stack'
-  end
-
-  def tags_count
-    tags.count
-  end
-end
 
 module YinxSt
   class << self
@@ -50,7 +34,7 @@ module YinxSt
       moved_book = all.select &:moved_book?
       changed_tags = all.select &:changed_tags?
 
-      @chart = MyChartkick.sample do |s|
+      @chart = MyChartkick.bundle do |s|
         s.my_line_chart all,
           title: "#{duration}总数变化",
           x: :dump_day,
@@ -124,73 +108,8 @@ module YinxSt
 
       end
 
-      self
     end
 
-    def _last_n_days n
-      batches = Batches.new(n + 1)
-      duration = "最近#{n}日"
 
-      @chart = MyChart.js do
-        material batches.all_notes
-        material batches.unwind_tags, name: :unwind_tags
-
-        select :yesterday do |note|
-          note.dump_id == batches.latest_id
-        end
-
-        select :yesterday, from: :unwind_tags do |note|
-          note.dump_id == batches.latest_id
-        end
-
-        select :changed_content do |note|
-          note.status != :remained
-        end
-
-        select :moved_book do |note|
-          note.moved_book?
-        end
-
-        select :changed_tags do |note|
-          note.changed_tags?
-        end
-
-        group_by :stack_book do |note|
-          stack = note.stack.nil? ? '' : "#{note.stack}/"
-          "#{stack}#{note.book}"
-        end
-
-        group_by(:stack){|note| note.stack or 'NO_STACK'}
-
-        group_by :number_of_tags do |note|
-          note.tags.count
-        end
-
-        plainBar :dump_day, name: "#{duration}总数变化", w: 1000, h:240, asc: :key, last: n
-        line :dump_day, :status, name: "#{duration}新建/修改/删除", from: :changed_content, w: 1000, h:240, asc: :key, keys: batches.time_line, last: n
-        line :dump_day, name: "#{duration}移动笔记本", from: :moved_book, w: 1000, h:240, asc: :key, keys: batches.time_line, last: n
-        line :dump_day, name: "#{duration}更改标签", from: :changed_tags, w: 1000, h:240, asc: :key, keys: batches.time_line, last: n
-
-        bar :stack_book, name: '目前最大的10个笔记本', from: :yesterday, w:1400 ,h: 540, desc: :count, first: 10
-        line :dump_day, :stack_book, name: "#{duration}笔记本体积变化", w: 1200, h:540, asc: :key, keys: batches.time_line, last: n
-
-        bar :stack, name: '目前最大的10个笔记本组', from: :yesterday, w:1400 ,h: 240, desc: :count, first: 10
-        line :dump_day, :stack, name: "#{duration}笔记本组体积变化", w: 1000, h:340, asc: :key, keys: batches.time_line, last: n
-
-        pie :tags, name: '目前使用最多的15个标签', from: :yesterday__from__unwind_tags, w:1400 ,h: 340, desc: :count, first: 15
-        bar :number_of_tags, name: '目前每篇笔记的标签数', from: :yesterday, w: 800, h: 240, asc: :key
-        line :dump_day, :tags, name: "#{duration}标签数变化", from: :unwind_tags, w: 1400, h:740, asc: :key, keys: batches.time_line, last: n
-
-      end
-
-      self
-    end
-
-    attr_reader :chart
-
-    def generate_html(erb_file=File.expand_path('../yinx_st/chart.erb', __FILE__))
-      template = File.read erb_file
-      html = ERB.new(template).result(binding)
-    end
   end
 end
